547b4dc15e91ac394aebf7d914191e895f87e4e7
ponnarasuice <ponnarasu.mcet@gmail.com>
2017-03-08 09:43:12 +0530

    welcome to git string added

--- sample/src/main/java/com/velociteg/sample/App.java
+++ sample/src/main/java/com/velociteg/sample/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! welome to git" );
         String sam ="companyid=101&page=1#2+sum-lo_n.ghttp://%%";
         String sam1 ="companyid=101&";
         System.out.println(StringUtils.containsAny(sam,
